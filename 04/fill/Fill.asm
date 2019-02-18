912a26c9f91beaac82fb298216ae306e3ff2a308
Shayla Lane <slane2@cocc.edu>
2019-02-18 14:03:55 -0800

    Fill and multiply

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,54 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(START)
+//Set Location to first screen value
+@SCREEN
+D=A
+@Location
+M=D
+// Is keyboard pressed
+@KBD
+D=M
+//If so turn black
+@BLACKLOOP
+D;JGT
+//If not turn white
+@WHITELOOP
+D;JEQ
+
+
+(BLACKLOOP)
+//Go to location and turn black
+@Location
+D=M
+A=D
+M=-1
+//Go to next address
+@Location
+D=M
+M=D+1
+D=M
+//Check if loop is done
+@KBD
+D=A-D
+@BLACKLOOP
+D;JGT
+
+(WHITELOOP)
+@Location
+D=M
+A=D
+M=0
+//Increment
+@Location
+D=M
+M=D+1
+D=M
+@KBD
+D=A-D
+@WHITELOOP
+D;JGT
+
+@START
+0;JMP
